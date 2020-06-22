25d4fa868955b33cfd973e306728fc21227110d3
Cheriele Mordido <eyelmordido@gmail.com>
2020-06-22 11:32:33 -0400

    Updates

---
+++ precache-manifest.6c60751c07ae46ed1181aeb8c4722961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24af41facd13aa9d754174d2c7580523",
+    "url": "/museumFinderProjectFive/index.html"
+  },
+  {
+    "revision": "85a16c8a7bdd26e6db3f",
+    "url": "/museumFinderProjectFive/static/css/main.b87b1ca4.chunk.css"
+  },
+  {
+    "revision": "dcf0504422d6ed72dc60",
+    "url": "/museumFinderProjectFive/static/js/2.c29af85e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/museumFinderProjectFive/static/js/2.c29af85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a16c8a7bdd26e6db3f",
+    "url": "/museumFinderProjectFive/static/js/main.fa659597.chunk.js"
+  },
+  {
+    "revision": "78c517c49bac5c33702b",
+    "url": "/museumFinderProjectFive/static/js/runtime-main.f2ff144a.js"
+  }
+]);
